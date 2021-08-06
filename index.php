1e1b75c2df03af2b9ae37f3f0fb704ca06be93cd
xcoders <57432765+xcoders-hub@users.noreply.github.com>
2021-08-06 22:06:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include 'dl.php';
 if($_GET['nm']){
     $new_file_name= base64_decode($_GET['nm']);
-    $finalfilename= $id.'-'.$new_file_name;
+    $finalfilename= $new_file_name;
     $fileurl= base64_decode($_GET['gd']);
     start($fileurl, $finalfilename, 10);
     sleep(6);
